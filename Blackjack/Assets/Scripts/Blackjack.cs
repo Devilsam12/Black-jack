c64b3cff6aac380a8ffcdf72e6ec9837d40c8e44
Samyak Meshram <sam.meshram18@gmail.com>
2021-03-20 23:21:24 +0530

    some touch-ups
    did some stuff

---
+++ Blackjack/Assets/Scripts/Blackjack.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackjack : MonoBehaviour
+{
+    public Sprite[] cardFaces;
+    public static string[] suites = new string[] { "C", "D", "H", "S" };
+    public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    [SerializeField] GameObject cardPrefab;
+    List<string> deck;
+    [SerializeField] float xOffset = 0.1f;
+    [SerializeField] float zOffset = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayCards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayCards()
+    {
+        deck = GenerateDeck();
+        Shuffle(deck);
+        Deal();
+
+    }
+
+    private void Deal()
+    {
+        foreach (var card in deck)
+        {
+            GameObject newCard = Instantiate(cardPrefab, new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z - zOffset), Quaternion.identity);
+            newCard.name = card;
+
+            xOffset += 0.5f;
+            zOffset += 0.5f;
+        }
+    }
+
+    public static List<string> GenerateDeck()
+    {
+        List<string> newDeck = new List<string>();
+        foreach(string suite in suites)
+        {
+            foreach(string value in values)
+            {
+                newDeck.Add(suite + value);
+            }
+        }
+
+        return newDeck;
+    }
+
+    void Shuffle<T>(List<T> list)
+    {
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = random.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
---
+++ Blackjack/Assets/UpdateCard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateCard : MonoBehaviour
+{
+    public Sprite cardFace;
+    public Sprite cardback;
+
+    private SpriteRenderer spriteRenderer;
+    private Blackjack blackjack;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> deck = Blackjack.GenerateDeck();
+        blackjack = FindObjectOfType<Blackjack>();
+
+        int i = 0;
+        foreach (var card in deck)
+        {
+            if (this.name == card)
+            {
+                cardFace = blackjack.cardFaces[i];
+                break;
+            }
+            i++;
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.sprite = cardFace;
+    }
+}
